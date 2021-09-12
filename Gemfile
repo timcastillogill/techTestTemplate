015a8128ccda50d86a23f7d508f687f3e5cc2828
timcastillogill <timgillviola@gmail.com>
2021-09-12 14:27:16 +0100

    Create RSPEC Test file with test and lib file to output a string

--- Gemfile
+++ Gemfile
@@ -9,3 +9,4 @@ gem 'rspec'
 gem 'rubocop'
 gem 'simplecov'
 gem 'simplecov-console'
+gem 'rubocop', require: false
---
+++ spec/tech_test_spec.rb
@@ -0,0 +1,7 @@
+require 'tech_test'
+
+describe 'Test Method' do
+  it 'returns Hello World' do
+    expect(test).to eq "Hello World"
+  end
+end
